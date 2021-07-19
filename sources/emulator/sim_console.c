f75a1e4509c45ba99fc2a5fcbd0186767a62da40
rcgoff <rcgoff@protonmail.ch>
2021-07-19 16:38:12 +0500

    ghub-fail:console status comments in sim_console

--- sources/emulator/sim_console.c
+++ sources/emulator/sim_console.c
@@ -2056,16 +2056,17 @@ return SCPE_OK;
 }
 
 static t_stat sim_os_ttrun (void)
-{
-	/*rc begin*/	
-	fprintf(sim_deb, "%s \n" , "sim_console: rc: sim_os_ttrun, we're here");
-	/*rc end*/
+{	
 if ((std_input) &&                                      /* If Not Background process? */
     (std_input != INVALID_HANDLE_VALUE) &&
     (!GetConsoleMode(std_input, &saved_mode) ||         /* Set mode to RAW */
      !SetConsoleMode(std_input, RAW_MODE)))
 	 /*rc begin*/	
 	fprintf(sim_deb, "%s \n" , "sim_console: rc: sim_os_ttrun, we're on error if-branch");
+	fprintf(sim_deb, "%s %d \n" , "sim_console: rc: sim_os_ttrun, GetConsoleMode: ",GetConsoleMode(std_input, &saved_mode));
+	fprintf(sim_deb, "%s %d \n" , "sim_console: rc: sim_os_ttrun, SetConsoleMode: ",SetConsoleMode(std_input, RAW_MODE));
+	fprintf(sim_deb, "%s %d \n" , "sim_console: rc: sim_os_ttrun, std_input as boolean: ",(std_input) );
+	fprintf(sim_deb, "%s %d \n" , "sim_console: rc: sim_os_ttrun, std_input: ",std_input );
 	/*rc end*/
     return SCPE_TTYERR;
 if (sim_log) {

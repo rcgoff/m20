dbdbbed70cc97c3ae73533f97fc9c9183bcde22b
rcgoff <rcgoff@protonmail.ch>
2025-03-11 16:33:17 +0500

    clean_win - the one command for both MS VS and minGW

--- sources/emulator/clean_win.bat
+++ sources/emulator/clean_win.bat
@@ -1,1 +1,13 @@
+@echo off
+nmake 1>NUL 2>NUL
+if ERRORLEVEL 1 goto mingw
 nmake -f makefile.win clean
+goto common_end
+:mingw
+nmake 1>NUL 2>NUL
+if ERRORLEVEL 1 goto no_make
+mingw32-make -f makefile.mgw clean
+:no_make
+echo ERROR: cannot find make utility, please check the paths
+:common_end
+
